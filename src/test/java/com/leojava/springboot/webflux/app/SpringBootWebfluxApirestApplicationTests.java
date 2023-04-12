7bbcf6fe6ffe73b6ce04f3052bae2baa37dda5b1
leonardo.caro <leonardo.caro@sofka.com.co>
2023-04-12 11:57:20 -0500

    Se crean metodo de busqueda por nombre en categoria y producto y se crean los test de las diferentes capacidades expuestas

--- src/test/java/com/leojava/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/leojava/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,6 +1,9 @@
 package com.leojava.springboot.webflux.app;
 
+import com.leojava.springboot.webflux.app.models.documents.Categoria;
 import com.leojava.springboot.webflux.app.models.documents.Producto;
+import com.leojava.springboot.webflux.app.models.services.ProductoService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Collections;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,6 +23,8 @@ class SpringBootWebfluxApirestApplicationTests {
 
 	@Autowired
 	private WebTestClient client;
+	@Autowired
+	private ProductoService service;
 
 
 
@@ -26,7 +36,86 @@ class SpringBootWebfluxApirestApplicationTests {
 				.exchange()
 				.expectStatus().isOk()
 				.expectHeader().contentType(MediaType.APPLICATION_JSON)
-				.expectBodyList(Producto.class);
+				.expectBodyList(Producto.class)
+				.consumeWith(response -> {
+					List<Producto> productos = response.getResponseBody();
+					productos.forEach(p -> {
+						System.out.println(p.getNombre());
+					});
+					Assertions.assertThat(productos.size()>0);
+				});
+	}
+
+	@Test
+	void verTest() {
+		Producto producto = service.findByNombre("Smart TV Samsung LCD").block();
+
+		client.get()
+				.uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.id").isNotEmpty()
+				.jsonPath("$.nombre").isEqualTo("Smart TV Samsung LCD")
+		;
+	}
+
+	@Test
+	public void crearTest() {
+		Categoria categoria = service.findCategoriaByNombre("Mobiliario").block();
+
+		Producto producto = new Producto("Mesa comedor", 605.00, categoria);
+
+		client.post().uri("/api/v2/productos")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.body(Mono.just(producto), Producto.class)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody(Producto.class)
+				.consumeWith(response -> {
+					Producto p = response.getResponseBody();
+					Assertions.assertThat(p.getId()).isNotEmpty();
+					Assertions.assertThat(p.getNombre()).isEqualTo("Mesa comedor");
+					Assertions.assertThat(p.getCategoria().getNombre()).isEqualTo("Mobiliario");
+				});
+	}
+
+	@Test
+	public void editarTest(){
+		Producto producto = service.findByNombre("Sony SmartPods TV LG LED").block();
+		Categoria categoria = service.findCategoriaByNombre("Electronico").block();
+		Producto productoEditado = new Producto("TV LG LED", 1200.00, categoria);
+
+		client.put().uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(Mono.just(productoEditado), Producto.class)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.id").isNotEmpty()
+				.jsonPath("$.nombre").isEqualTo("TV LG LED")
+				.jsonPath("$.categoria.nombre").isEqualTo("Electronico");
+	}
+
+	@Test
+	public void eliminarTest(){
+		Producto producto = service.findByNombre("HP Impresora RecargaContinua").block();
+		client.delete()
+				.uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+				.exchange()
+				.expectStatus().isNoContent()
+				.expectBody().isEmpty();
+
+		client.get()
+				.uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectBody().isEmpty();
 	}
 
 }
